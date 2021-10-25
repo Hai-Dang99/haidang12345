4902c08b47cca836bf2efe88fcf77b2d59d72e7b
Hai-Dang99 <haidang250899@gmail.com>
2021-10-25 18:38:43 +0700

    Add project files.

---
+++ haidang99/Migrations/202110251133055_hee.cs
@@ -0,0 +1,26 @@
+﻿namespace haidang99.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class hee : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.SinhViens",
+                c => new
+                    {
+                        MaSinhVien = c.String(nullable: false, maxLength: 128),
+                        TenSinhVien = c.String(),
+                    })
+                .PrimaryKey(t => t.MaSinhVien);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.SinhViens");
+        }
+    }
+}
---
+++ haidang99/Models/LapTrinhQuanLyDBcontext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace haidang99.Models
+{
+    public class LapTrinhQuanLyDBcontext : DbContext
+    {
+
+        public LapTrinhQuanLyDBcontext() : base("LapTrinhQuanLyDBcontext")
+        {
+        }
+
+        public DbSet<Student> Students { get; set; }
+        public DbSet<SinhVien> SinhViens { get; set; }
+
+
+
+    }
+}
+//DESKTOP - 62IMTN6\SQLEXPRESS
